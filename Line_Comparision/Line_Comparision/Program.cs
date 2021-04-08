85682d6ca9bbff073fd295992833d51141c4a918
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-04-08 19:04:14 +0530

    [Sanju] ADD : compare both lines are equal or not

--- Line_Comparision/Line_Comparision/Program.cs
+++ Line_Comparision/Line_Comparision/Program.cs
@@ -5,26 +5,39 @@ namespace Line_Comparision
     class Program
     {
         /// <summary>
-        /// Calculate length of a line
+        /// Calculate length of two lines
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("********Welcome to Line Comparision Program**********");
 
-                double x1, y1, x2, y2;
-                Console.WriteLine("Enter the cordinates (x1,y1) :");
-                x1 = Convert.ToInt32(Console.ReadLine());
-                y1 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Enter the cordinates (x2,y2) :");
-                x2 = Convert.ToInt32(Console.ReadLine());
-                y2 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine(calculateDistance(x1, y1, x2, y2));
-            }
-            //Function to calculate the length of a line
-            private static double calculateDistance(double x1, double y1, double x2, double y2)
-            {
-                return (double)Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
-            }
+            //Variable declaration
+            double x1, y1, x2, y2, x3, y3, x4, y4;
+            //For line 2
+            Console.WriteLine("Enter the cordinates (x1,y1) :");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the cordinates (x2,y2) :");
+            x2 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+            //For line 2
+            Console.WriteLine("Enter the cordinates (x3,y3) :");
+            x3 = Convert.ToInt32(Console.ReadLine());
+            y3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the cordinates (x4,y4) :");
+            x4 = Convert.ToInt32(Console.ReadLine());
+            y4 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(calculateDistance(x2, y3, x4, y4));
+
+            //Calculate line 1 and line 2 distance
+            double distance1 = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            double distance2 = Math.Sqrt(Math.Pow((x4 - x3), 2) + Math.Pow((y4 - y3), 2));
+
+            //Compare both lines
+            Console.WriteLine(distance1.Equals(distance2));
+
+
+        }
     }
  }
