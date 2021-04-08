893ce59d55d34e5459e229908f0d3c389b230640
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-04-08 18:46:42 +0530

    [Sanju] ADD : calculate length of a line

--- Line_Comparision/Line_Comparision/Program.cs
+++ Line_Comparision/Line_Comparision/Program.cs
@@ -4,9 +4,27 @@ namespace Line_Comparision
 {
     class Program
     {
+        /// <summary>
+        /// Calculate length of a line
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("********Welcome to Line Comparision Program**********");
-        }
+
+                double x1, y1, x2, y2;
+                Console.WriteLine("Enter the cordinates (x1,y1) :");
+                x1 = Convert.ToInt32(Console.ReadLine());
+                y1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the cordinates (x2,y2) :");
+                x2 = Convert.ToInt32(Console.ReadLine());
+                y2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(calculateDistance(x1, y1, x2, y2));
+            }
+            //Function to calculate the length of a line
+            private static double calculateDistance(double x1, double y1, double x2, double y2)
+            {
+                return (double)Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            }
     }
-}
+ }
